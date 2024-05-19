c29e212df7e906bb6b8614fbc3a8ae097e6a6936
Mulokozi-silencer <143081224+Mulokozi-silencer@users.noreply.github.com>
2024-05-19 20:08:26 +0300

    index1.php stands for home page of the system
    Its a home page of the system.

---
+++ index1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Voting</title>
+</head>
+<body>
+    <section class="menu">
+        <div class="nav">
+           <div class="logo"><h1>G4<b>Online Voting System</b></h1></div>
+           <ul>
+                 <li><a class="active" href="#home">Home</a></li>
+                 <li><a href="register.php">Register</a></li>
+                 <li><a href="aboutus.php">About Us</a></li>
+           </ul> 
+           <div>
+            <a href="login.php"><input class="signin" type="submit" value="Sign In" name="signin"></a>            
+           </div>
+        </div>
+    </section>
+    <section class="grid">
+        <div class="content">
+           <div class="content-left">
+             <div class="info">
+                <h2>Modern<br>Online Voting System</h2>
+                <p>Trust in Technology, From Anywhere to everywhere: Make Your Mark Online<br>
+                    Your Vote, Your Right: Ensure Your Voice Is Heard Today</p>
+             </div>
+           </div>
+        </div>
+    </section>
+</body>
+</html>
